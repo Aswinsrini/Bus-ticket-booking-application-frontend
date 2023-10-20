3f8d4f7a9d9d52e51147433c575889c9c75de429
Aswin <aswinsrinivasan2004@gmail.com>
2023-10-20 21:36:39 +0530

    intial state of the application

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Signup from "./components/Singup/Signupp";
+import Login from "./components/Login/Login";
+import { Routes, Route } from "react-router-dom";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Navbar />} />
+        </Routes>
+      </div>
+    );
+  }
+}
+
+export default App;
